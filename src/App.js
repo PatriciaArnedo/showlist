b07c61c173c05b47a3fb2c8d8693a600d89d23b5
PatriciaArnedo <paaarnedo@gmail.com>
2021-04-29 02:39:41 -0400

    added header and root components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import RootContainer from './components/rootcontainer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+        <RootContainer/>
     </div>
   );
 }
---
+++ src/components/rootcontainer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from './header.js'
+
+const RootContainer = () => {
+
+    return(
+        <Header/>
+    )
+}
+
+export default RootContainer
